3ec6b88b3754bb8fcd4bdcc44782b8c5528d8a1b
Rajalakshmi <120360428+rajistat@users.noreply.github.com>
2023-06-19 06:50:39 +0530

    Creating a Database using Script

--- relational-database/salon/salon.sh
+++ relational-database/salon/salon.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+#creating applocation coad
+
+PSQL="psql -X --username=postgres --dbname=postgres --tuples-only -c"
+
+#Drop database
+
+$($PSQL "DROP DATABASE salon_test" &> /dev/null )
+STATUS=$?
+if [[ $STATUS != 0 ]]
+then
+  echo "database does not exist..."
+else
+  echo "deleting existing database.."
+fi
+
+#Create database
+
+$($PSQL "CREATE DATABASE salon_test" &> /dev/null )
+STATUS=$?
+if [[ $STATUS != 0 ]]
+then
+  echo "database already exist......"
+else
+  echo "connect to database..."
+fi
