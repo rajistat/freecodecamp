47c0389a06ceba3b6a290131999e7771ed8dc0ee
Rajalakshmi <120360428+rajistat@users.noreply.github.com>
2023-07-01 15:36:28 +0530

    Update database.sh

--- relational-database/worldcup/database.sh
+++ relational-database/worldcup/database.sh
@@ -1,1 +1,33 @@
+#/ bin/bash
+#set variables for users and database
+if [[ $1 = 'test' ]]
+then
+DB="postgres"
+WC_DB="worldcuptest"
+USR="postgres"
+else
+DB="postgres"
+WC_DB="worldcup"
+USR="freecodecamp"
+fi
+PSQL="psql -X --username=$USR --dbname=$DB --no-align --tuples-only -c"
 
+#Drop database 
+$($PSQL "DROP DATABASE $WC_DB" &> /dev/null)
+STATUS=$?
+if [[ $STATUS != 0 ]]
+then 
+echo "database does not exist...."
+else
+echo "deleting existing database........."
+fi
+
+#creating database
+$($PSQL "CREATE DATABASE $WC_DB" &> /dev/null )
+STATUS=$?
+if [[ $STATUS != 0 ]]
+then
+echo "database already exit........"
+else
+echo "connect to database......."
+fi
